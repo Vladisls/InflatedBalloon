07cae777f4ef1f353ac4233363f951380f9566f7
Vladislav <v.stafg@gmail.com>
2015-03-22 23:56:32 +0200

    IB kaust
    Work done by Joonas

---
+++ Testkeskkond/ib/application/views/home/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+ "http://www.w3.org/1999/xhtml">
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <meta charset="utf-8">
+        <title>InflatedBalloon</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!--[if lt IE 9]>
+          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link rel="shortcut icon" href="/bootstrap/img/favicon.ico">
+        <link rel="apple-touch-icon" href="/bootstrap/img/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x114.png">
+    </head>
+<body>
+
+<div class="jumbotron">
+  <h1>Testing</h1> 
+  <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive,
+  mobile-first projects on the web.</p> 
+</div>
+    
+<div class="container-fluid">
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">InflatedBalloon</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+
+       
+      </ul>
+      <form class="navbar-form navbar-left">
+          <button type="button" class="btn btn-default">Kandidaadid</button>
+          <button type="button" class="btn btn-default">Erakonnad</button>
+          <button type="button" class="btn btn-default">Tulemused</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+          
+                 <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">My Account <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Vote</a></li>
+            <li><a href="#">My Vote History</a></li>
+            <li><a href="#">My applications</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Log Out</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+
+
+
+
+    
+<div class="container">
+  <div class="jumbotron">
+        <div class="product">               
+                <div class="product_image">
+                   <img class="img-responsive" src="resources/card.png" alt=""/>
+                </div>
+             </div>
+     </div>
+</div>
+
+</body>
+</html>
